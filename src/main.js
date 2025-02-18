9c56bbfe39dfb25ee1994a55a84beeae36829d89
kephalosk <watchfromtheshore@gmail.com>
2025-02-18 20:28:15 +0100

    add InfoCard Component

---
+++ src/main.js
@@ -0,0 +1,45 @@
+let infoCard, profileCard;
+
+initializeApp().then();
+
+async function initializeApp() {
+    await fetchData();
+    await fetchComponents();
+    renderData();
+    addEventListener();
+}
+
+async function fetchData() {
+}
+
+async function fetchComponents() {
+    await fetchInfoCard();
+    await fetchProfileCard();
+}
+
+async function fetchInfoCard() {
+    await fetch('./src/components/InfoCard.html')
+        .then(response => response.text())
+        .then(data => {
+            infoCard = data;
+        })
+        .catch(error => console.error('Error while fetching InfoCard component:', error));
+}
+
+async function fetchProfileCard() {
+    await fetch('./src/components/ProfileCard.html')
+        .then(response => response.text())
+        .then(data => {
+            profileCard = data;
+        })
+        .catch(error => console.error('Error while fetching ProfileCard component:', error));
+}
+
+function renderData() {
+    const app = document.getElementById('app');
+    console.log(infoCard);
+    app.innerHTML += infoCard;
+}
+
+function addEventListener() {
+}
